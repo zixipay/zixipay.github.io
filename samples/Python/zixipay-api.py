8517ce88ed8c2e1ec20a718d534a7c4f58389d4f
ZixiPay Wallet <62507892+zixipay@users.noreply.github.com>
2021-06-20 16:10:08 +0300

    Create zixipay-api.py

---
+++ samples/Python/zixipay-api.py
@@ -0,0 +1,40 @@
+import requests
+import urllib.parse
+import hmac
+import hashlib
+import time
+import json
+
+#
+# Zixipay API endpoint call
+#
+# endpoint: string, endpoint you want to call
+# params: array, enpoint parameters
+# uid: string, User ID
+# apikey: string, API Access Key
+#
+
+def ZixiPay_ApiCall(endpoint, params, apikey):
+    apiurl = 'https://api.zixipay.com/apiv2/'    # API endpoint URL
+    payload = urllib.parse.urlencode(params)    # create urlencoded payload
+    signature = hmac.new(bytes(apikey, 'latin-1'), bytes(payload, 'latin-1'), hashlib.sha256).hexdigest().lower()    # generate signature
+    params.append(('sig', signature))    # add signature to the end of api parameters array
+    result = requests.post(apiurl+endpoint, data = params, headers = {"Content-Type": "application/x-www-form-urlencoded"})
+    if result == None:
+        return False
+    else:
+        return json.loads(result.text)
+
+
+uid = ''          # ZixiPay User ID
+apikey = ''       # ZixiPay API Access Key
+
+# API call parameters 
+prms = [
+    ('uid',  uid),
+    ('ts',   int(time.time()))
+]
+
+# getbalances call
+result = ZixiPay_ApiCall('getbalances', prms, apikey)
+print(result)
